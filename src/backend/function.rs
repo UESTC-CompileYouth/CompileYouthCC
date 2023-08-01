6cb6b8646d64899b291aba4fd56e203f0ddc85e9
wei-huan <1520799054@qq.com>
2023-08-01 10:58:55 +0800

    refactor: delete useless jump

--- src/backend/function.rs
+++ src/backend/function.rs
@@ -244,7 +244,7 @@ impl Function {
             .block_mapping()
             .get(&llvm_function.entry_bb_id())
             .unwrap();
-        if real_entry_block_id != 1 {
+        if real_entry_block_id != 1 + entry_block_id {
             entry_block.push_back(Box::new(JumpInstr::new_jump(
                 BLOCK_LABEL_PREFIX.to_string() + real_entry_block_id.to_string().as_str(),
             )));
