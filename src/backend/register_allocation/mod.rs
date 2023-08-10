ec74b177984034629677b832ef2b8d47b91d3eac
iku-iku-iku <2548340423@qq.com>
2023-08-10 13:50:38 +0800

    fix f32 cmp

--- src/backend/register_allocation/mod.rs
+++ src/backend/register_allocation/mod.rs
@@ -737,15 +737,14 @@ pub(crate) fn register_allocate(func: &mut Function) {
 
             // 5. assign color
             // while !stk.is_empty() {
-            stk.sort_by_key(|x| (*x * 100000000) as u64);
-            // stk.sort_by(|x, y| {
-            //     ig.nodes
-            //         .get(x)
-            //         .unwrap()
-            //         .spill_cost()
-            //         .partial_cmp(&ig.nodes.get(y).unwrap().spill_cost())
-            //         .unwrap_or(std::cmp::Ordering::Equal)
-            // });
+            stk.sort_by(|x, y| {
+                ig.nodes
+                    .get(x)
+                    .unwrap()
+                    .spill_cost()
+                    .partial_cmp(&ig.nodes.get(y).unwrap().spill_cost())
+                    .unwrap_or(std::cmp::Ordering::Equal)
+            });
             for &n in stk.iter().rev() {
                 // let n = stk.pop().unwrap();
                 let c = ig.assign_color(n);
